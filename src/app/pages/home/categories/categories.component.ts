ac8cf0ab23dc4c8048789a307cc3acb120ea55c7
Demetre <136166143+DemetreD@users.noreply.github.com>
2026-01-22 10:51:22 -0800

    Style categories component

--- src/app/pages/home/categories/categories.component.ts
+++ src/app/pages/home/categories/categories.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
 export class CategoriesComponent {
+  categories = [
+    { name: 'Phones', icon: 'assets/phones.png' },
+    { name: 'Smart Watches', icon: 'assets/watches.png' },
+    { name: 'Cameras', icon: 'assets/cameras.png' },
+    { name: 'Headphones', icon: 'assets/headphones.png' },
+    { name: 'Computers', icon: 'assets/computers.png' },
+    { name: 'Gaming', icon: 'assets/gaming.png' },
+  ];
 
 }
