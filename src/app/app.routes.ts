fbf4a2d3c7ddbc74ffb86c6975f35aa756b28835
Demetre <136166143+DemetreD@users.noreply.github.com>
2026-01-22 07:18:46 -0800

    Update routes and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,9 @@ import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { AdminComponent } from './pages/admin/admin.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -17,6 +20,9 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'admin', component: AdminComponent },
+  { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'blog', component: BlogComponent },
 
   { path: '**', redirectTo: '' },
 ];
