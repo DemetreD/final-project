33aad75cb4f9cced4ad2434f7b214136285a4444
Demetre <136166143+DemetreD@users.noreply.github.com>
2026-01-22 23:38:06 -0800

    Add discounts section

--- src/app/pages/home/discounts/discounts.component.ts
+++ src/app/pages/home/discounts/discounts.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Product } from '../../../models/product';
+import { ProductCardComponent } from '../../../product-card/product-card.component';
+import { ProductsService } from '../../../services/product.service';
 @Component({
   selector: 'app-discounts',
-  imports: [],
+  imports: [CommonModule,ProductCardComponent],
   templateUrl: './discounts.component.html',
   styleUrl: './discounts.component.css'
 })
 export class DiscountsComponent {
+   items: Product[] = [];
+
+  constructor(private productsService: ProductsService) {
+    this.productsService.getAll().subscribe(all => {
+      this.items = all.slice(0, 4);
+    });
+  }
 
 }
+
+
+
--- src/app/pages/home/feature-tiles/feature-tiles.component.ts
+++ src/app/pages/home/feature-tiles/feature-tiles.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-feature-tiles',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './feature-tiles.component.html',
   styleUrl: './feature-tiles.component.css'
 })
 export class FeatureTilesComponent {
-
+tiles = [
+    {
+      title: 'Popular Products',
+      desc: 'iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.',
+      image: 'assets/products/watch.png',
+      dark: false,
+      link: '/products'
+    },
+    {
+      title: 'Ipad Pro',
+      desc: 'Pad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.',
+      image: 'assets/products/ipad.png',
+      dark: false,
+      link: '/products'
+    },
+    {
+      title: 'Samsung Galaxy',
+      desc: 'Pad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.',
+      image: 'assets/products/phone2.png',
+      dark: false,
+      link: '/products'
+    },
+    {
+      title: 'Macbook Pro',
+      desc: 'Pad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.',
+      image: 'assets/mac.png',
+      dark: true,
+      link: '/products'
+    }
+  ];
 }
+
+
+
